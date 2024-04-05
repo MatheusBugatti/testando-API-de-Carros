c30c93e045ce5e5c6905ccd4f4a2deb582e35432
matheus <matheus.info@mazatarraf.com.br>
2024-04-05 10:52:18 -0300

    chore(marcas.service) Requisição HTTP para API

---
+++ src/app/services/marcas.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { IMarcas } from '../models/marcas.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarcasService {
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  private _url = environment.apiUrl;
+  
+  //fazer requisao http para api atraves de carro/marcas
+  getMarcaCarro(){
+    return this._http.get<IMarcas>(`${this._url}/carros/marcas`)
+  }
+
+}

bdceeb9e15dc690e93651d1e537be37a200f919d
matheus <matheus.info@mazatarraf.com.br>
2024-04-05 10:37:49 -0300

    perf(app)Aumnetando a performance e adicionando rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: ' ',
         component: HomeComponent
     }
+
 ];

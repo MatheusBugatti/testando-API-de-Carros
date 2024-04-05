4b241c50d8a601d6bf85547e23ef3092e4da2ea1
matheus <matheus.info@mazatarraf.com.br>
2024-04-05 15:06:37 -0300

    feat(home)Filtrando pelo Front

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,12 +3,14 @@ import { IMarcas } from '../../models/marcas.model';
 import { CardComponent } from '../../components/card/card.component';
 import { MarcasService } from '../../services/marcas.service';
 import { Subscription, catchError, subscribeOn } from 'rxjs';
+import { PoFieldModule, PoSelectOption } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CardComponent,
+    PoFieldModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -16,6 +18,9 @@ import { Subscription, catchError, subscribeOn } from 'rxjs';
 export class HomeComponent implements OnInit{
 
   marcasCarro: IMarcas[] = [];
+  marcasCarroBackup: IMarcas[] = [];
+  opcoesMarcas: PoSelectOption[] = [];
+  
 
   //injetando o a service MarcasServices em _marcaCarrosService
   constructor(private _marcaCarrosService: MarcasService){}
@@ -27,6 +32,7 @@ export class HomeComponent implements OnInit{
   //toda vez que inicializa o projeto ele faz esse role
   ngOnInit(): void {
    this.listarMarcaCarro();
+   
   }
 
   //metodo/função que lista o carro
@@ -43,6 +49,28 @@ export class HomeComponent implements OnInit{
       ).subscribe((res: IMarcas ) => {
         //se tiver marcasCarro ele vai alimentar a variavel
         this.marcasCarro = this.marcasCarro.concat(res);
-      })
+        this.marcasCarroBackup = this.marcasCarro;
+        if(this.marcasCarro.length > 0){
+          this.popularSelect();
+        }
+    } )
+  }
+    
+  popularSelect(){
+    this.opcoesMarcas = this.marcasCarro.map((x) => {
+      return ({label: x.nome , value: x.codigo})
+
+    })
+    // 
+  }
+
+  getOpcao(event: any){
+    console.log(event);
+    let opcaoEscolhida = event;
+
+    this.marcasCarro = this.marcasCarroBackup.filter((x) => {
+      return x.codigo == opcaoEscolhida
+    })
+    
   }
 }

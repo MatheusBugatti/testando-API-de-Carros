75e74ed2db6c6f8b1c26d8c9d147a39b8cb429d3
matheus <matheus.info@mazatarraf.com.br>
2024-04-12 11:06:39 -0300

    feat(menu) Implementando o menu

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PoMenuItem, PoMenuModule, PoPageModule, PoToolbarModule } from '@po-ui/ng-components';
+
+@Component({
+  selector: 'menu',
+  standalone: true,
+  imports: [
+    PoMenuModule,
+    PoToolbarModule, 
+    PoPageModule,
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+  menuItems : Array<PoMenuItem> = [
+    {label: 'Home', icon: 'po-icon-star', shortLabel: 'Welcome', action: () => this.navigateTo('welcome')},
+    {label: 'Tipo veiculo', icon: 'po-icon po-icon-home', shortLabel: 'Tipo veiculo', action: () => this.navigateTo('')}
+  ]
+
+  constructor(private _router: Router){}
+
+  private navigateTo(route: string): void {
+    this._router.navigate([route]);
+  }
+}

5c0572376315e090720d76aca1576f6d7dd83ae0
matheus <matheus.info@mazatarraf.com.br>
2024-04-12 11:05:27 -0300

    chore(app.component) implementando novas Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    }
+    },
+    {
+        path: 'welcome',
+        component: WelcomeComponent
+    },
 
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }

2a2f8fa3ee889f1a7043251a229d7df0e795cb30
matheus <matheus.info@mazatarraf.com.br>
2024-04-12 11:08:05 -0300

    chore(veiculos.option)

---
+++ src/app/shared/veiculos-options/veiculos.options.ts
@@ -0,0 +1,6 @@
+import { PoSelectOption } from "@po-ui/ng-components";
+
+export const veiculosOptions: PoSelectOption[] = [
+    {label: "Caminhões", value: "caminhoes"},
+    {label: "Carros", value: "carros"}
+]
